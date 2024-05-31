6e3d6d2fb3b1589039e7634a6b350b410511e5b1
Caiasso <guilhermecaiasso@gmail.com>
2024-05-31 15:21:36 -0300

    Working with arrays

--- script.js
+++ script.js
@@ -290,4 +290,34 @@ console.log(percentages);
 // end of lecture 15
 
 // lecture 16
+// Create an array containing all the neighbouring countries of a country of your choice. Choose a country which has at least 2 or 3 neighbours. Store the array into a variable called neighbours.
 
+// At some point, a new country called 'Utopia' is created in the neighbourhood of your selected country, so add it to the end of the neighbours array.
+
+// Unfortunately, after some time the new country is dissolved, so remove it from the end of the array.
+
+// If the neighbours array does not include the country 'Germany', log to the console: 'Probably not a central european country :D'.
+
+// Change the name of one of your neighbouring countries. To do that, find the index of the country in the neighbours array, and then use that index to change the array at that index position. For example, you can search for 'Sweden' in the array, and then replace it with 'Republic of Sweden'.
+
+const neighbourCountries = [`Norway`, `Denmark`, `Finland`];
+const showNeighbours = function(){console.log(neighbourCountries)};
+showNeighbours();
+
+neighbourCountries.push(`Utopia`);
+showNeighbours();
+
+neighbourCountries.pop();
+showNeighbours();
+
+const centralEuropeanCheck = function() {console.log(!neighbourCountries.includes(`Germany`) ? `Probabbly not a central european country :D` : `Probably a central european country ^^`)};
+
+centralEuropeanCheck();
+neighbourCountries.unshift(`Germany`);
+centralEuropeanCheck();
+neighbourCountries.shift();
+
+neighbourCountries[neighbourCountries.indexOf(`Norway`)] = `Constitutional Monarchy of Norway`;
+showNeighbours();
+
+// end of lecture 16
